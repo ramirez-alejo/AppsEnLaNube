8c1b5c1aab49b4f807b640f7706bbee61243b79f
Alejandro Ramirez <a.ramirez1123@uniandes.edu.co>
2024-05-05 21:11:45 -0400

    Isolated components scaling (#25)
    * Actualizar docker compose para poder correr los servicios de manera independiente
    
    * Actualizar docker compose para usar un folder local creado para el nfs
    
    * Incrementar conexione spara poder realizar prueba de usuarios concurrentes
    
    * Incrementar las conexion hasta 400
    
    * Agregar gunicorn
    
    * Agregar gcp sorage
    
    * Add the always flag so docker starts the containers on host re-start

--- app-drones-worker/app.py
+++ app-drones-worker/app.py
@@ -1,6 +1,7 @@
 import os, json, pika, sys
 from sqlalchemy import create_engine
-from azure.storage.blob import BlobServiceClient
+from google.cloud import storage
+from google.oauth2 import service_account
 from modelos.video import Video
 from modelos.usuario import Usuario
 from sqlalchemy.orm import  sessionmaker
@@ -21,11 +22,25 @@ channel = connection.channel()
 channel.queue_declare(queue='files')
 
 
-
 postgres_host = os.environ.get("POSTGRES_HOST", 'localhost')
 postgres_port = os.environ.get("POSTGRES_PORT", '5432')
 postgres_user = os.environ.get("POSTGRES_USER", 'postgres')
 postgres_password = os.environ.get("POSTGRES_PASSWORD", 'postgres')
+
+
+gcp_credentials = {
+    "type": os.environ.get("GCP_CREDENTIALS_TYPE", "service_account"),
+    "project_id": os.environ.get("GCP_CREDENTIALS_PROJECT_ID", ),
+    "private_key_id": os.environ.get("GCP_CREDENTIALS_PRIVATE_KEY_ID",),
+    "private_key": str(os.environ.get("GCP_CREDENTIALS_PRIVATE_KEY")).replace('\\n', '\n'),
+    "client_email": os.environ.get("GCP_CREDENTIALS_CLIENT_EMAIL"),
+    "client_id": os.environ.get("GCP_CREDENTIALS_CLIENT_ID"),
+    "auth_uri": os.environ.get("GCP_CREDENTIALS_AUTH_URI", "https://accounts.google.com/o/oauth2/auth"),
+    "token_uri": os.environ.get("GCP_CREDENTIALS_TOKEN_URI", "https://oauth2.googleapis.com/token"),
+    "auth_provider_x509_cert_url": os.environ.get("GCP_CREDENTIALS_AUTH_PROVIDER_X509_CERT_URL", "https://www.googleapis.com/oauth2/v1/certs"),
+    "client_x509_cert_url": os.environ.get("GCP_CREDENTIALS_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.environ.get("GCP_CREDENTIALS_UNIVERSE_DOMAIN", "googleapis.com")
+}
 
 def get_engine(user, passwd, host, port, db):
     url = f"postgresql://{user}:{passwd}@{host}:{port}/{db}"
@@ -40,7 +55,6 @@ init_db()
 #Make sure the tmp folder exists
 if not os.path.exists('/tmp'):
     os.makedirs('/tmp')
-
 
 
 def file_processed(ch, method, properties, body):
@@ -58,7 +72,12 @@ def file_processed(ch, method, properties, body):
             session.commit()
             print('Processing video')
 
-            videoPath = f'/nfsshare/{message["filename"]}'
+            # Download video
+            print('Downloading video')
+            videoPath = f'/tmp/{message["filename"]}'
+            with open(videoPath, 'wb') as f:
+                f.write(download_blob('drones-app-storage', '', message['filename']))
+            print('Video downloaded')
 
             # Process video
             #using shell command cut the video to 20 seconds
@@ -68,11 +87,19 @@ def file_processed(ch, method, properties, body):
             os.system(f'ffmpeg -i /tmp/logo-{message["filename"]} -i logos/IDRL.png -filter_complex "[1:v]scale=200:-1[logo];[0:v][logo]overlay=10:10:enable=\'between(t,18,20)\'" /tmp/processed-{message["filename"]}')
 
             print('Uploading processed video')
+
+            upload_blob('drones-app-storage', f'/tmp/processed-{message["filename"]}', f'processed-{message["filename"]}')
+
+            # Afher uploading the file to storage, we can delete the local files
+            os.remove(f'/tmp/processed-{message["filename"]}')
+            os.remove(f'/tmp/logo-{message["filename"]}')
+            os.remove(f'/tmp/aspect-{message["filename"]}')
+            os.remove(f'/tmp/short-{message["filename"]}')
             
             #copy to the nfs from /tmp/
-            os.system(f'cp /tmp/processed-{message["filename"]} /nfsshare/processed-{message["filename"]}')
+            #os.system(f'cp /tmp/processed-{message["filename"]} /processed-{message["filename"]}')
             print('Processed video uploaded')
-            video.processed_url = f'/nfsshare/processed-{message["filename"]}'
+            video.processed_url = f'/processed-{message["filename"]}'
             video.status = 'completed'
             session.commit()
             print('Video updated')
@@ -86,6 +113,28 @@ def file_processed(ch, method, properties, body):
         raise e
         
 
+
+def download_blob(bucket_name, source_folder, blob_name):
+    storage_client = get_storage_client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_folder + blob_name)
+    blob_bytes = blob.download_as_bytes()
+    
+    print("-> Downloaded storage object {} from bucket {}".format(blob_name, bucket_name))
+    return blob_bytes
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    storage_client = get_storage_client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    generation_match_precondition = 0
+    blob.upload_from_filename(source_file_name, if_generation_match = generation_match_precondition)
+
+    print("\n-> Updaload storage object {} to bucket {} to {}".format(blob.name, bucket_name, destination_blob_name))
+
+def get_storage_client():
+    credentials = service_account.Credentials.from_service_account_info(gcp_credentials)
+    return storage.Client(credentials=credentials)
 
 channel.basic_consume(queue='files', on_message_callback=file_processed, auto_ack=True)
 
